57d2de5ae6c63fe3e6e892de7305ee159aed562d
Charles Proust <konoha33@hotmail.fr>
2025-07-24 09:41:49 +0200

    start crud

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,10 +13,13 @@ final class DashboardAdminController extends AbstractController
 {
     #[Route('/dashboardAdmin', name: 'app_dashboard_admin')]
     #[IsGranted("ROLE_ADMIN")]
-    public function dashboardAdmin(): Response
+    public function dashboardAdmin(UserRepository $userRepo): Response
     {
+        $users = $userRepo->findAll();
+
         return $this->render('dashboard_admin/index.html.twig', [
             'controller_name' => 'DashboardAdminController',
+            'users' => $users
         ]);
     }
 }
